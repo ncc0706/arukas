8c88c69f93df934a197bac24f89e2f1280b74277
ncc0706 <ncc0706@gmail.com>
2017-06-20 11:02:04 +0800

    return map data

--- src/main/java/io/arukas/controller/DemoController.java
+++ src/main/java/io/arukas/controller/DemoController.java
@@ -4,12 +4,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
-public class Demo01 {
+public class DemoController {
 
 	@GetMapping("/hello")
 	public String sayHello(@RequestParam(value = "message", defaultValue = "World") String message){
 		return "Hello " + message;
 	}
-	
+
+	@GetMapping(value = "/data")
+	public Map<String, String> mapData(){
+		Map<String,String> map = new HashMap<String,String>();
+		map.put("content", "hello spring boot");
+		return map;
+	}
+
 }
